54596bd2d5c2d958d1d4b09496f5adaccc75dc1d
bryanpdl <bryan@ui8.net>
2019-11-27 16:05:25 -0800

    rendered product cards w/ json data

---
+++ client/src/Components/Products/Products.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import '../Cards/Cards.css';
+
+class Products extends Component {
+    constructor() {
+        super();
+        this.state = {
+            products: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/api/ui8')
+            .then(res => res.json())
+            .then(products => this.setState({ products }, () => console.log('Products fetched.', products)))
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Products</h2>
+                {this.state.products.map(product =>
+                    <div className="Card">
+                        <h3>{product.name}</h3>
+                        <p>{product.price}</p>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
+
+export default Products;
